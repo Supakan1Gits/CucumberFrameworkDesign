d464a6ff61c18ec3bea215c6b728ea7b3b08cedb
Supakan1Gits <mochana24@gmail.com>
2023-01-03 17:36:44 -0500

    First commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,20 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="./,src/test/resouces/features",
+		glue="step_definitions",
+		dryRun=true 
+		// when it's true, cucumber only run the scenario steps that have not been implemented
+		)
+public class TestRunner {
+	
+	
+	
+
+}
